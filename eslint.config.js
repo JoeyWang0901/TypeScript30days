cbf6a7ed00576c87ef8e4df2693d00a2b78a453f
JoeyWang0901 <joey.wang@wisepointtech.com>
2025-11-06 19:03:21 +0800

    remove eslint-plugin-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -16,13 +16,11 @@ module.exports = [
     },
     plugins: {
       "@typescript-eslint": tsPlugin, // TypeScript 專屬插件
-      prettier: require("eslint-plugin-prettier"), // Prettier 插件
     },
     rules: {
       ...js.configs.recommended.rules, // ESLint 基本規則
       ...tsPlugin.configs.recommended.rules, // TypeScript 推薦規則
       ...prettier.rules, // 正確使用 prettier 配置
-    //   "prettier/prettier": "error", // Prettier 格式錯誤會顯示
       semi: ["error", "always"], // 行尾一定要有分號
       quotes: ["error", "double"], // 用雙引號
       "no-var": "error", // 不准用 var
