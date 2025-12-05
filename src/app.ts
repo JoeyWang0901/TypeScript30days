5b3ae6b9b7e88b44a48642aa5a167b57c8d15188
JoeyWang0901 <joey.wang@wisepointtech.com>
2025-12-05 11:51:43 +0800

    D18: Upgrade project logging by replacing console.log with Pino

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,25 @@ import todoRoutes from "./routes/todoRoutes";
 import authRoutes from "./routes/authRoutes";
 import uploadRoutes from "./routes/uploadRoutes";
 import dotenv from "dotenv";
+import logger from "./utils/logger";
+import pinoHttp from "pino-http";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+// 測試不同級別的日誌
+logger.info("這是一般資訊");
+logger.debug("這是除錯訊息");
+logger.warn("這是警告");
+logger.error("這是錯誤");
+
 app.use("/api/todos", todoRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/upload", uploadRoutes);
+
+app.use(pinoHttp({ logger }));
 
 app.get("/", (req, res) => {
   res.send("Hello, iThome 2025!");
@@ -22,9 +32,9 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 AppDataSource.initialize()
   .then(() => {
-    console.log("Database connected");
+    logger.info("Database connected");
     app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
+      logger.info(`🚀 Server running on http://localhost:${PORT}`);
     });
   })
   .catch((error) => {
